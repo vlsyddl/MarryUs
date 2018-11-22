92df1e2782994f6680ffa42fefb95cba188170ea
임유신 <limys0104@gmail.com>
2018-11-22 11:35:14 +0900

    [2018.11.22 임유신] - 공지사항 페이징

--- marryus/src/main/java/kr/co/marryus/repository/domain/Page.java
+++ marryus/src/main/java/kr/co/marryus/repository/domain/Page.java
@@ -1,10 +1,28 @@
 package kr.co.marryus.repository.domain;
 
 public class Page {
+	private String select;
+	private String text;
 	private int pageNo;
 	private int begin;
-	
-	
+	private int end;
+
+	public String getSelect() {
+		return select;
+	}
+
+	public void setSelect(String select) {
+		this.select = select;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
 	public int getPageNo() {
 		return pageNo;
 	}
@@ -14,9 +32,24 @@ public class Page {
 	}
 
 	public int getBegin() {
-		return (pageNo-1)*15;
+		return (pageNo - 1) * 10;
 	}
+
 	public void setBegin(int begin) {
 		this.begin = begin;
+	}
+
+	public int getEnd() {
+		return pageNo * 10;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	@Override
+	public String toString() {
+		return "NoticePage [select=" + select + ", text=" + text + ", pageNo=" + pageNo + ", begin=" + begin + ", end="
+				+ end + "]";
 	}
 }
