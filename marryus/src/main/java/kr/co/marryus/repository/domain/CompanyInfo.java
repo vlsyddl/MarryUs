3bc864c2654d676ce25bddbf03ab401c71ec92ca
DESKTOP-UBIDC58\eunbee <dq0318@gmail.com>
2018-11-27 15:21:04 +0900

    [2018.11.27 김은비] 도메인 올립니다.

--- marryus/src/main/java/kr/co/marryus/repository/domain/CompanyInfo.java
+++ marryus/src/main/java/kr/co/marryus/repository/domain/CompanyInfo.java
@@ -4,6 +4,8 @@ package kr.co.marryus.repository.domain;
 
 
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
@@ -12,5 +14,22 @@ public class CompanyInfo {
 	private int memNo;
 	private String comInfoContent;
 	private String comInfoProfile;
+	private String comInfoName;
+	private String comInfoPhone;
+	private String comInfoAddr;
+	private String comInfoAddrDetail;
+	private String comInfoType;
+	private String comInfoOkay;
+	
+	
+	private String comFileName;
+	private String comFilePath;
+	
+	
+	private String aucionType;
+	private Date auction_sdate;
+	private Date auction_edate;
+	private int tenderBudget;
+	private Date tenderRegdate;
 	
 }
