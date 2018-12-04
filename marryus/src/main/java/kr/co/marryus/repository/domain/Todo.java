80a4fee82ccb9f386b54f0f034960bd679ce9bfd
DESKTOP-UBIDC58\eunbee <dq0318@gmail.com>
2018-12-04 15:51:02 +0900

    [2018.12.04 김은비] 다시 todo xml, todo vo 올립니다.

--- marryus/src/main/java/kr/co/marryus/repository/domain/Todo.java
+++ marryus/src/main/java/kr/co/marryus/repository/domain/Todo.java
@@ -1,5 +1,19 @@
 package kr.co.marryus.repository.domain;
 
-public class Todo {
+import lombok.Data;
 
+@Data
+public class Todo {
+	private String todoSchedule;
+	private String todoContent;
+	private int todoDday;
+	private String todoCategory;
+	private int todoNo;
+	private int memNo;
+	private String todoCheck;
+	private String todoDelete;
 }
+
+
+
+   
